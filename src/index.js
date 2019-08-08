23706a83c8e91165c4c510cae1697b03c9c0de8a
https://julian-figueroa@bitbucket.org <julianfigueroa@MacBook-Pro-de-Julian.local>
2019-08-08 17:20:20 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import { StoreProvider } from './Store';
+import './index.css';
+import HomePage from './HomePage';
+import FavPage from './FavPage';
+import { Router } from '@reach/router';
+
+ReactDOM.render(
+  <StoreProvider>
+    <Router>
+      <App path='/'>
+        <HomePage path='/' />
+        <FavPage path='/faves' />
+      </App>
+    </Router>
+  </StoreProvider>,
+  document.getElementById('root')
+);
